d1fd005c43754ac5487cc124f1c7ff09c7139e37
sharanpeter <petersharan900@gmail.com>
2022-01-19 21:29:57 +0530

    build react done

---
+++ client/build/precache-manifest.3b8d4223c8adbc5e0e5a1a0bb0cf6188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3376566b3c99b2bdbf24ab041b2c93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8213e9a7827ca96597bb",
+    "url": "/static/css/main.fb9f1093.chunk.css"
+  },
+  {
+    "revision": "b0ffc48a006ef7ad79ff",
+    "url": "/static/js/2.80da8f8e.chunk.js"
+  },
+  {
+    "revision": "edffb87fb5452d5ab97f74c5d7d9520a",
+    "url": "/static/js/2.80da8f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8213e9a7827ca96597bb",
+    "url": "/static/js/main.b0a7c938.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
